25fc35074fd259639fbac27019eeefeaba37b5b1
er suresh <developersureshthakur@gmail.com>
2023-10-13 14:26:56 +0530

    http request added for generate password

--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,15 @@
+//config
+axios.defaults.baseURL = "http://localhost:8082"
+
 // Modules
 import {signup} from '../modules/firebase.js'
 
 // Selectors
 const signupForm = document.querySelector("#signup-form")
 const result = document.querySelector("#result")
+const generatePasswordBtn = document.querySelector("#generate-password")
+const passwordField = document.querySelector("#password-field")
+const loader = document.querySelector("#loader")
 
 // Functions
 signupForm.onsubmit = async (e)=>{
@@ -17,4 +23,21 @@ signupForm.onsubmit = async (e)=>{
 
     result.innerHTML = "Registration failed !"
     result.className = "text-rose-500 font-semibold"
+}
+
+
+generatePasswordBtn.onclick = async () =>{
+   try{
+        loader.style.display = "block"
+        const {data} = await axios.get("/generate-password")
+        passwordField.value = data.password
+        passwordField.type = "text"
+   }
+   catch(err){
+        alert("err.message")
+   }
+   finally{
+        loader.style.display = "none"
+        
+   }
 }
